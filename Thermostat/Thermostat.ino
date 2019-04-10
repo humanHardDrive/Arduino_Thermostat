bf5fd87ab49678376747e9904dd233a002408dc9
Sam Stratter <humanHardDrive@gmail.com>
2019-04-09 22:31:29 -0400

    Flush out some methods to send messages

--- Thermostat/Thermostat.ino
+++ Thermostat/Thermostat.ino
@@ -1,12 +1,27 @@
 #include "ThermostatStation.h"
 
-ThermoStation thermostat;
+#define DISCOVERY_PIN   7
+
+#define DEBOUNCE_TIME   5
+
+ThermoStation thermostat(4,5,6);
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(115200);
 
+  pinMode(DISCOVERY_PIN, INPUT_PULLUP);
 }
 
+uint32_t discoveryPinDebounceTime = 0;
+
 void loop() {
+  if(!digitalRead(DISCOVERY_PIN))
+  {
+      if((discoveryPinDebounceTime + DEBOUNCE_TIME) < millis())
+        thermostat.startDiscovery(1500);
+  }
+  else
+    discoveryPinDebounceTime = millis();
+  
   thermostat.background();
 }
--- Thermostat/ThermostatStation.cpp
+++ Thermostat/ThermostatStation.cpp
@@ -1,11 +1,25 @@
 #include "ThermostatStation.h"
 
-ThermoStation::ThermoStation()
-{ 
+ThermoStation::ThermoStation(uint8_t fanPin, uint8_t heatPin, uint8_t coolPin)
+{
+  //Set all of the control pins to outputs
+  pinMode(fanPin, OUTPUT);
+  pinMode(heatPin, OUTPUT);
+  pinMode(coolPin, OUTPUT);
+
+  //Turn off all of the outputs
+  digitalWrite(fanPin, LOW);
+  digitalWrite(heatPin, LOW);
+  digitalWrite(coolPin, LOW);
 }
 
 ThermoStation::~ThermoStation()
 {
+}
+
+void ThermoStation::background()
+{
+  BaseStation::background();
 }
 
 int ThermoStation::write(const void* buf, uint16_t len)
