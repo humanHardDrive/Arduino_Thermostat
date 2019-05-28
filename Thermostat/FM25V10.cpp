cd8a091d119f2d32b52f484a7f94e9b40b82ccae
Sam Stratter <humanharddrive@gmail.com>
2019-05-27 20:02:50 -0400

    Create control methods for FRAM

--- Thermostat/FM25V10.cpp
+++ Thermostat/FM25V10.cpp
@@ -1,1 +1,93 @@
 #include "FM25V10.h"
+
+#include <SPI.h>
+
+FM25V10::FM25V10(byte csPin) :
+  m_CSPin(csPin)
+{
+}
+
+FM25V10::~FM25V10()
+{
+}
+
+byte FM25V10::read(uint32_t address)
+{
+  address &= ADDRESS_MASK;
+  byte retVal = 0xFF;
+
+  digitalWrite(m_CSPin, LOW);
+
+  SPI.transfer(READ_OP_CODE);
+  SPI.transfer(((byte*)&address)[2]);
+  SPI.transfer(((byte*)&address)[1]);
+  SPI.transfer(((byte*)&address)[0]);
+  retVal = SPI.transfer(0x00);
+
+  digitalWrite(m_CSPin, HIGH);
+
+  return retVal;
+}
+
+byte FM25V10::read(uint32_t address, byte* buffer, uint32_t len)
+{
+  address &= ADDRESS_MASK;
+
+  digitalWrite(m_CSPin, LOW);
+
+  SPI.transfer(READ_OP_CODE);
+  SPI.transfer(((byte*)&address)[2]);
+  SPI.transfer(((byte*)&address)[1]);
+  SPI.transfer(((byte*)&address)[0]);
+
+  for (uint32_t i = 0; i < len; i++)
+    buffer[i] = SPI.transfer(0x00);
+
+  digitalWrite(m_CSPin, HIGH);
+}
+
+
+void FM25V10::write(uint32_t address, byte val)
+{
+  address &= ADDRESS_MASK;
+
+  writeEnable();
+
+  digitalWrite(m_CSPin, LOW);
+
+  SPI.transfer(WRITE_OP_CODE);
+  SPI.transfer(((byte*)&address)[2]);
+  SPI.transfer(((byte*)&address)[1]);
+  SPI.transfer(((byte*)&address)[0]);
+  SPI.transfer(val);
+
+  digitalWrite(m_CSPin, HIGH);
+}
+
+void FM25V10::write(uint32_t address, byte* buffer, uint32_t len)
+{
+  address &= ADDRESS_MASK;
+
+  writeEnable();
+
+  digitalWrite(m_CSPin, LOW);
+
+  SPI.transfer(WRITE_OP_CODE);
+  SPI.transfer(((byte*)&address)[2]);
+  SPI.transfer(((byte*)&address)[1]);
+  SPI.transfer(((byte*)&address)[0]);
+
+  for(uint32_t i = 0; i < len; i++)
+    SPI.transfer(buffer[i]);
+
+  digitalWrite(m_CSPin, HIGH);
+}
+
+
+void FM25V10::writeEnable()
+{
+  digitalWrite(m_CSPin, LOW);
+  SPI.transfer(WRITE_ENABLE_OP_CODE);
+  digitalWrite(m_CSPin, HIGH);
+}
+
