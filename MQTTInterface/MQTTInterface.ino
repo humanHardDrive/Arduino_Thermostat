b30d042e44b5ad2bbc21d29af84cde8d3cf0e6fc
Sam Stratter <humanharddrive@gmail.com>
2019-10-09 20:39:24 -0400

    Add saved info structure to store network info

--- MQTTInterface/MQTTInterface.ino
+++ MQTTInterface/MQTTInterface.ino
@@ -1,11 +1,28 @@
 #include <PubSubClient.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define SERIAL_STX  0x55
+#define SERIAL_ATX  0xAA
 
+#define MAX_NAME_LENGTH 32
+
+struct SAVE_INFO
+{
+  char sNetworkName[MAX_NAME_LENGTH];
+  char sNetworkPass[MAX_NAME_LENGTH];
+
+  char sServerName[MAX_NAME_LENGTH];
+  char sServerPass[MAX_NAME_LENGTH];
+
+  uint32_t checksum;
+};
+
+SAVE_INFO SavedInfo;
+
+void setup() 
+{
+  Serial.begin(115200);
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
 }
