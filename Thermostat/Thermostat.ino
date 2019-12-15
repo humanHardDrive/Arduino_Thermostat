566b467e5f57b298be7fcd80492534832405b1bf
Sam Stratter <humanharddrive@gmail.com>
2019-12-15 17:40:58 -0500

    Update pin definitions

--- Thermostat/Thermostat.ino
+++ Thermostat/Thermostat.ino
@@ -13,19 +13,23 @@
 #define VERSION     F("v0.0")
 
 //Arduino Pins
+#define IO_EXP_RST_PIN  13
+#define ESP_RST_PIN     12
+#define ESP_EN_PIN      11
+#define ESP_ST_PIN      10
 #define LCD_RS_PIN      9
 #define LCD_EN_PIN      8
 #define LCD_D4_PIN      7
 #define LCD_D5_PIN      6
 #define LCD_D6_PIN      5
 #define LCD_D7_PIN      4
+#define HEARTBEAT_PIN   3
 #define IO_EXP_INT_PIN  2
 
-#define IO_EXP_CS_PIN   A0
-#define IO_EXP_RST_PIN  A1
 #define I2C_SDA         A4
 #define I2C_SCL         A5
 #define LOCAL_TEMP_PIN  A6
+#define BATT_MON_PIN    A7
 
 //Variables
 ESPInterface    espInterface;
