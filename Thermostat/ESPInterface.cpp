af4b2711806f5ec1b4f81cf38d7e72eede103021
Sam Stratter <humanharddrive@gmail.com>
2019-12-15 16:57:23 -0500

    Implement serial methods for ESP interface

--- Thermostat/ESPInterface.cpp
+++ Thermostat/ESPInterface.cpp
@@ -10,4 +10,81 @@ ESPInterface::~ESPInterface()
 
 void ESPInterface::background(uint8_t c)
 {
+  if ((millis() - m_LastRXTime) > 10 &&
+      m_ParseState != WAITING_FOR_STX)
+    m_ParseState = WAITING_FOR_STX;
+
+  m_LastRXTime = millis();
+
+  (this->*m_StateFn[m_ParseState])(c);
+}
+
+void ESPInterface::sendCommand(uint8_t cmd, void* buf, uint8_t len)
+{
+}
+
+void ESPInterface::WaitingForSTXState(uint8_t c)
+{
+  if (c == SERIAL_STX)
+  {
+    m_ParseState = WAITING_FOR_CMD;
+
+    //Reset the current command code and the number of
+    //bytes received
+    m_CurrentCommand =
+      m_CommandDataCount = 0;
+    memset(m_CurrentCommandBuf, 0, sizeof(m_CurrentCommandBuf));
+  }
+}
+
+void ESPInterface::WaitingForCMDState(uint8_t c)
+{
+  m_CurrentCommand = c;
+  m_ParseState = WAITING_FOR_LEN;
+}
+
+void ESPInterface::WaitingForLenState(uint8_t c)
+{
+  //TODO: limit this number to the buffer size
+  m_CurrentCommandLen = c;
+
+  if (m_CurrentCommandLen)
+    m_ParseState = WAITING_FOR_DATA;
+  else
+    //If there's no data to be sent, just jump to the end state
+    m_ParseState = WAITING_FOR_ETX;
+}
+
+void ESPInterface::WaitingForDataState(uint8_t c)
+{
+  //Add data to the buffer
+  m_CurrentCommandBuf[m_CommandDataCount] = c;
+  m_CommandDataCount++;
+
+  if (m_CommandDataCount >= m_CurrentCommandLen)
+    m_ParseState = WAITING_FOR_ETX;
+}
+
+void ESPInterface::WaitingForETXState(uint8_t c)
+{
+  if (c == SERIAL_ETX)
+  {
+    m_ParseState = WAITING_FOR_STX;
+
+    /*//If a handler exists for this command code, call it
+      if (m_CmdHandler.find(m_CurrentCommand) != m_CmdHandler.end())
+      m_CmdHandler[m_CurrentCommand](m_CurrentCommandBuf);
+      else if (m_CmdHandler.find(INVALID_CMD) != m_CmdHandler.end())
+      m_CmdHandler[INVALID_CMD](m_CurrentCommandBuf);*/
+  }
+  else if (c == SERIAL_STX)
+  {
+    WaitingForSTXState(c);
+
+    /*//If a handler exists for this command code, call it
+      if (m_CmdHandler.find(m_CurrentCommand) != m_CmdHandler.end())
+      m_CmdHandler[m_CurrentCommand](m_CurrentCommandBuf);
+      else if (m_CmdHandler.find(INVALID_CMD) != m_CmdHandler.end())
+      m_CmdHandler[INVALID_CMD](m_CurrentCommandBuf);*/
+  }
 }
--- Thermostat/ESPInterface.h
+++ Thermostat/ESPInterface.h
@@ -3,6 +3,13 @@
 
 #include <Arduino.h>
 
+#include "Msgs.h"
+
+#define SERIAL_STX  0x55
+#define SERIAL_ETX  0xAA
+
+#define COMMAND_BUFFER_LEN    128
+
 class ESPInterface
 {
   public:
@@ -14,6 +21,40 @@ class ESPInterface
     void sendCommand(uint8_t cmd, void* buf, uint8_t len);
 
   private:
+    enum PARSE_STATE
+    {
+      WAITING_FOR_STX = 0, //Waiting for the STX character to come over serial
+      WAITING_FOR_CMD,  //Collecting the 2 letter command code
+      WAITING_FOR_LEN,  //Getting the total number of characters in the data frame
+      WAITING_FOR_DATA, //Actual data for the command
+      WAITING_FOR_ETX,  //Waiting for the character to restart transmission
+      ALL_PARSE_STATES
+    };
+
+    uint8_t m_ParseState = WAITING_FOR_STX;
+
+    uint32_t m_LastRXTime = 0;
+
+    uint8_t m_CurrentCommand = NO_CMD;
+    uint8_t m_CurrentCommandLen;
+    uint8_t m_CommandDataCount;
+    uint8_t m_CurrentCommandBuf[COMMAND_BUFFER_LEN];
+
+    void WaitingForSTXState(uint8_t);
+    void WaitingForCMDState(uint8_t);
+    void WaitingForLenState(uint8_t);
+    void WaitingForDataState(uint8_t);
+    void WaitingForETXState(uint8_t);
+
+    void (ESPInterface::*m_StateFn[ALL_PARSE_STATES])(uint8_t) = {
+      &ESPInterface::WaitingForSTXState,
+      &ESPInterface::WaitingForCMDState,
+      &ESPInterface::WaitingForLenState,
+      &ESPInterface::WaitingForDataState,
+      &ESPInterface::WaitingForETXState
+    };
+
+    
 };
 
 #endif
