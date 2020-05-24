cefac63f2ec9a473c499e796c4fe371d2e8eca58
humanharddrive <humanharddrive@gmail.com>
2020-05-24 13:29:20 -0400

    Update IO map. Blink heartbeat while awake.

--- RemoteSensor/RemoteSensor.ino
+++ RemoteSensor/RemoteSensor.ino
@@ -10,16 +10,19 @@
 #define WAKE_TIME_S           (10)
 #define WAKE_TIME_MS          (WAKE_TIME_S*1000)
 
-#define CHARGE_SATUS_PIN    1
-#define HB_LED_PIN          2
-#define CHARGER_STATUS_PIN  3
-#define BATT_LED_PIN        4
-#define UP_BTN_PIN          5
-#define SCL_PIN             9
-#define SDA_PIN             10
-#define DOWN_BTN_PIN        12
-#define CENTER_BTN_PIN      13
-#define BATTERY_STATUS_PIN  14
+#define DEBOUNCE_TIME         15
+
+#define CHARGE_STATUS_PIN     1
+#define HB_LED_PIN            2
+#define CHARGER_STATUS_PIN    3
+#define BATT_LED_PIN          4
+#define UP_BTN_PIN            5
+#define SCL_PIN               9
+#define SDA_PIN               10
+#define DOWN_BTN_PIN          12
+#define CENTER_BTN_PIN        13
+#define BATTERY_STATUS_PIN    14
+#define SLEEP_PIN             15
 
 #define TEMP_SENSE_PIN  A0
 
@@ -28,20 +31,24 @@ SensorDevice sensor(TEMP_SENSE_PIN, 100);
 uint32_t ulSleepTime = DEFAULT_SLEEP_TIME_US;
 uint32_t ulWakeTimeStart = 0;
 
-void setup() 
+void setup()
 {
   pinMode(UP_BTN_PIN, INPUT_PULLUP);
   pinMode(DOWN_BTN_PIN, INPUT_PULLUP);
-  pinMode(LEFT_BTN_PIN, INPUT_PULLUP);
-  pinMode(RIGHT_BTN_PIN, INPUT_PULLUP);
   pinMode(CENTER_BTN_PIN, INPUT_PULLUP);
 
-  pinMode(SLEEP_PIN, OUTPUT);
-  pinMode(WAKE_PIN, OUTPUT);
+  pinMode(CHARGE_STATUS_PIN, INPUT);
+  pinMode(CHARGER_STATUS_PIN, INPUT);
+  pinMode(BATTERY_STATUS_PIN, INPUT);
 
+  pinMode(HB_LED_PIN, OUTPUT);
+  pinMode(BATT_LED_PIN, OUTPUT);
+  pinMode(SLEEP_PIN, OUTPUT);
+
+  digitalWrite(HB_LED_PIN, HIGH);
+  digitalWrite(BATT_LED_PIN, LOW);
   digitalWrite(SLEEP_PIN, LOW);
-  digitalWrite(SLEEP_PIN, HIGH);
-  
+
   ulWakeTimeStart = millis();
 }
 
@@ -49,23 +56,40 @@ void goToSleep()
 {
   //Sleep
   //Setup the reset latch
-  digitalWrite(WAKE_PIN, LOW); //Off
+  digitalWrite(HB_LED_PIN, LOW); //Off
   digitalWrite(SLEEP_PIN, HIGH); //Before on
   //Turn off WiFi
   WiFi.mode(WIFI_OFF);
   //Deep sleep
-  ESP.deepSleep(ulSleepTime, WAKE_RF_DISABLED); 
-  
+  ESP.deepSleep(ulSleepTime, WAKE_RF_DISABLED);
+
   //Wakeup
   //Clear the reset latch
   digitalWrite(SLEEP_PIN, LOW); //Off
-  digitalWrite(WAKE_PIN, HIGH); //Before on
+  digitalWrite(HB_LED_PIN, HIGH); //Before on
   ulWakeTimeStart = millis();
+}
+
+unsigned long ulLastHBUpdate = 0;
+void updateStatusLED()
+{
+  if ((millis() - ulLastHBUpdate) > 1000)
+  {
+    if (digitalRead(HB_LED_PIN))
+      digitalWrite(HB_LED_PIN, LOW);
+    else
+      digitalWrite(HB_LED_PIN, HIGH);
+
+    ulLastHBUpdate = millis();
+  }
 }
 
 void loop()
 {
   sensor.update();
-  if((millis() - ulWakeTimeStart) > WAKE_TIME_MS)
+
+  updateStatusLED();
+
+  if ((millis() - ulWakeTimeStart) > WAKE_TIME_MS)
     goToSleep();
 }
